63e6452e703352648ba9153512ad83c94b0e7747
Camilo Aristizabal Bedoya <camilo_aristizabal@outlook.com>
2024-11-13 23:08:07 -0500

    mejora del menu

--- src/app/hotel/pages/layout-page/layout-page.component.ts
+++ src/app/hotel/pages/layout-page/layout-page.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
+  styleUrls: ['./layout-page.component.css'],
 })
 export class LayoutPageComponent {
   // Array de objetos que representan los elementos del sidebar
   public sidebarItems = [
-    { label: 'patients', icon: 'label', url: './patients'},
+    { label: 'patients', icon: 'patient_list', url: './patients'},
   ];
 }
